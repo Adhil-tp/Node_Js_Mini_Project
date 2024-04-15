cde443fb3a6bff6d0f9801729b17c637f744b36f
Adhil-tp <mohamedadhiltp1944@gmail.com>
2024-04-15 12:08:05 +0530

    register page server side validation completed

--- index.js
+++ index.js
@@ -3,44 +3,36 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const session = require("express-session");
+const { error } = require("console");
 
 const userDetails = "userList.json";
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: "this is a secret",
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-      maxAge: 60000 * 60,
-    },
+    saveUninitialized: true,
+    httpOnly: true,
+    secure: true,
+    resave: true
   })
 );
 
-app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/home", (req, res) => {
-  res.render("home.ejs", {
-    title: "HomePage",
-  });
-});
 
-app.get("/this", (req, res) => {
-  console.log(req.session);
-  console.log(req.sessionID);
-  req.session.visited = true;
-});
 
 app.get("/", (req, res) => {
   console.log(req.session);
   console.log(req.sessionID);
   res.render("home.ejs", { title: "HomePage" });
 });
+
+
 app.get("/login", (req, res) => {
   res.render("login.ejs", { title: "LoginPage" });
 });
@@ -50,20 +42,69 @@ app.post("/login", (req, res) => {
   res.redirect("/home");
 });
 
+
+
+// register page 
+
 app.get("/registerPage", (req, res) => {
-  res.render("registerPage", { title: "registerPage" });
+  let duplicateError = req.session.duplicateFound
+  req.session.duplicateFound = ''
+
+  let { nameError, emailError, passError, confirmPassError } = req.session
+  req.session.nameError = ''
+  req.session.emailError = ''
+  req.session.passError = ''
+  req.session.confirmPassError = ''
+
+  res.render("registerPage", { title: "registerPage", duplicateError, nameError, emailError, passError, confirmPassError });
 });
 
+
 app.post("/registerPage", (req, res) => {
+
+  const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)[^ ]{8,20}$/;
+  const emailRegex = /^[a-zA-Z0-9]{5,30}@[a-zA-Z]{2,7}.[a-zA-Z]{1,5}$/;
   // console.log(JSON.stringify(req.body));
+  console.log(req.body)
+  const { userName, userMail, createPass, confirmPass } = req.body
+
+
+
+
+  if (userName.length <= 2) {
+    req.session.nameError = "Name must be atleast 3 letters"
+    return res.redirect("/registerPage")
+  }
+  if (!emailRegex.test(userMail)) {
+    req.session.emailError = "field must be in email format"
+    console.log('redirected to reg page')
+    return res.redirect("/registerPage")
+  }
+  if (!passRegex.test(createPass)) {
+    req.session.passError = "the password must contain 8 character , 1 number , 1 uppercase , 1 lowercase and 1 special character"
+    return res.redirect("/registerPage")
+  }
+  if (createPass != confirmPass) {
+    req.session.confirmPassError = "this pass must match the previous field"
+    return res.redirect("/registerPage")
+  }
+
+
+  // console.log(`name : ${userName}  | mail : ${userMail} | pass : ${createPass}`)
+
+
 
   const userObj = JSON.parse(fs.readFileSync("userList.json", "utf8"));
 
   const duplicateUser = userObj.find((user) => {
+    let cleanedUserName = user.userName.trim().toLowerCase()
+    let cleanedUserMail = user.userMail.trim().toLowerCase()
     return (
-      user.userName == req.body.userName || user.userMail == req.body.userMail
+      cleanedUserName == userName.trim().toLowerCase() ||
+      cleanedUserMail == userMail.trim().toLowerCase()
     );
   });
+
   if (duplicateUser == undefined) {
     // const parsedUserList = userObj;
     userObj.push(req.body);
@@ -74,10 +115,14 @@ app.post("/registerPage", (req, res) => {
         console.log(err);
       }
     });
-    res.json({ status: false });
+    // console.log("Session before no duplicate:", req.session.duplicateFound);
+    req.session.duplicateFound = ''
+    // console.log("Session after no duplicate:", req.session.duplicateFound);
+    res.redirect('/home');
   } else {
-    console.log("found duplicate");
-    res.json({ status: true });
+    console.log("Found duplicate")
+    req.session.duplicateFound = "A user is found with same credetials"
+    res.redirect("/registerPage")
   }
 
   // res.redirect("home");
--- public/js/registerPage.js
+++ public/js/registerPage.js
@@ -1,70 +1,55 @@
-const passError = document.querySelector(".pass-error");const button = document.getElementById("register");
+const passError = document.querySelector(".pass-error");
+const button = document.getElementById("register");
+const emailError = document.querySelector('.email-error-static')
 const confirmError = document.querySelector(".confirm-error");
 const existingUser = document.querySelector(".existing-user");
 // const fs = require("fs");
 
-button.addEventListener("click", async (event) => {
-  event.preventDefault();
+button.addEventListener("click", (event) => {
+  // event.preventDefault();
   const userName = document.getElementById("userName").value;
   const userMail = document.getElementById("userMail").value;
   const newPassWord = document.getElementById("createPass").value;
   const confirmPassWord = document.getElementById("confirmPass").value;
 
-  //   console.log(typeof userName);
-  //   console.log(typeof userMail);
-  //   console.log(newPassWord.length);
 
   if (userName == "" || userMail == "" || newPassWord == "") {
     alert("enter your user Credentials to move forward");
-    // return;
+    event.preventDefault()
+    return;
   } else {
     const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)[^ ]{8,20}$/;
-    const emailRegex = /^[a-zA-Z0-9]{5,30}@[a-zA-Z]{5,10}.[a-zA-Z]{1,5}$/;
-    let isMailTrue = false,
-      isPassTrue = false,
-      isConfirmPassTrue = false;
-    if (passRegex.test(newPassWord)) {
-      console.log("pass valid");
-      passError.style.display = "none";
-      isPassTrue = true;
-    } else {
+    const emailRegex = /^[a-zA-Z0-9]{5,30}@[a-zA-Z]{2,7}.[a-zA-Z]{1,5}$/;
+
+    passError.style.display = "none"
+    confirmError.style.display = "none"
+
+    if (userName.length <= 2) {
+      event.preventDefault()
+    }
+
+
+    if (!emailRegex.test(userMail)) {
+      console.log("field must be in email format");
+      emailError.style.display = "block"
+      event.preventDefault()
+      // return
+    }
+
+    if (!passRegex.test(newPassWord)) {
+      console.log("pass not valid");
       passError.style.display = "block";
+      event.preventDefault()
+      // return
     }
 
-    if (emailRegex.test(userMail)) {
-      console.log("email format");
-      isMailTrue = true;
-    } else {
-      console.log("not a email");
-    }
-
-    if (newPassWord !== confirmPassWord) {
-      console.log("password must match");
+    if (newPassWord != confirmPassWord) {
+      console.log("password not match");
       confirmError.style.display = "block";
-    } else {
-      confirmError.style.display = "none";
-      isConfirmPassTrue = true;
+      event.preventDefault()
+      // return
     }
-    console.log(isMailTrue, isConfirmPassTrue, isPassTrue);
-    if (isMailTrue && isConfirmPassTrue && isPassTrue) {
-      //   console.log("HERE");
-      const response = await fetch("/registerPage", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-          userName,
-          userMail,
-          newPassWord,
-          confirmPassWord,
-        }),
-      });
-      const data = await response.json();
-      if (data.status == true) {
-        existingUser.style.display = "block";
-      } else if (data.status == false) {
-        existingUser.style.display = "none";
-        window.location.href = "/home";
-      }
-    }
+
   }
+  return
 });
