0d5a6770cfe5b77509e0e3674f4108cb3248b0ff
Adhil-tp <mohamedadhiltp1944@gmail.com>
2024-04-22 15:22:17 +0530

    validation done

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/registerPage", (req, res) => {
     // console.log("Session before no duplicate:", req.session.duplicateFound);
     req.session.duplicateFound = ''
     // console.log("Session after no duplicate:", req.session.duplicateFound);
-    res.redirect('/home');
+    res.redirect('/');
   } else {
     console.log("Found duplicate")
     req.session.duplicateFound = "A user is found with same credetials"
